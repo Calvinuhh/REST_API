0c6436d153313a936f651053965da9647ff09d71
Calvin Ulis Howard Howard <Calvin.uhh@gmail.com>
2024-12-31 17:17:52 -0500

    Modificando script.ts

--- src/script.ts
+++ src/script.ts
@@ -4,8 +4,6 @@ import path from "path";
 export default function createUploadsDir() {
   const uploadsDir = path.join(__dirname, "uploads");
 
-  console.log(uploadsDir);
-
   if (!fs.existsSync(uploadsDir)) {
     fs.mkdirSync(uploadsDir);
   }
