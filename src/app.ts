5b3d118fe5d1de4bea7d9e4bfb6b44e1efff4a02
Calvin Ulis Howard Howard <Calvin.uhh@gmail.com>
2024-12-31 16:30:53 -0500

    Agregando cors

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,17 @@
 import express, { json, urlencoded } from "express";
 import router from "./router/router";
+import cors from "cors";
+
+process.loadEnvFile();
+const { CLIENT_URL } = process.env;
 
 const server = express();
 
+server.use(
+  cors({
+    origin: CLIENT_URL,
+  })
+);
 server.use(json());
 server.use(urlencoded({ extended: true }));
 
