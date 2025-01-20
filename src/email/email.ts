c3347630af958fd50f683f3a324114561f16a56b
Calvin Ulis Howard Howard <Calvin.uhh@gmail.com>
2025-01-20 18:42:59 -0500

    Corrigiendo errores con sendgrid en produccion

--- src/email/email.ts
+++ src/email/email.ts
@@ -1,32 +1,27 @@
-import nodemailer from "nodemailer";
+import sgMail from "@sendgrid/mail";
 
-const { MAIL_HOST, MAIL_PORT, MAIL_USER, MAIL_PASSWORD, SERVER_URL, EMAIL } =
-  process.env as {
-    [key: string]: string;
-  };
+const { SENDGRID_API_KEY, SERVER_URL, EMAIL } = process.env as {
+  [key: string]: string;
+};
 
-const transport = nodemailer.createTransport({
-  host: MAIL_HOST,
-  port: Number(MAIL_PORT),
-  auth: {
-    user: MAIL_USER,
-    pass: MAIL_PASSWORD,
-  },
-});
+sgMail.setApiKey(SENDGRID_API_KEY);
 
 export const enviarEmail = async (
   name: string,
   email: string,
   token: string
 ) => {
-  await transport.sendMail({
-    from: `${EMAIL} - REST API`,
-    to: email,
-    subject: "Confirm your account",
-
-    html: `
-            <h2>Hi ${name}! click on the next link to confirm your account: </h2>
-            <a href="${SERVER_URL}/auth/${token}">Confirm Account</a>
-        `,
-  });
+  try {
+    await sgMail.send({
+      to: email,
+      from: EMAIL,
+      subject: "Confirm your account",
+      html: `
+    <h2>Hi ${name}! click on the next link to confirm your account: </h2>
+    <a href="${SERVER_URL}/auth/${token}">Confirm Account</a>
+    `,
+    });
+  } catch (error) {
+    throw Error("Error sending email");
+  }
 };
